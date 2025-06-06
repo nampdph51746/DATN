d9d4992cd9b33fd69664ed7d69f3444f0b015c5a
ThanhSonPH52957 <sont18573@gmail.com>
2025-06-06 12:08:02 +0700

    CRUD: cinemas, cities, countries

--- app/Models/Cinema.php
+++ app/Models/Cinema.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // ✅ Thêm dòng này
 
 class Cinema extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes; // ✅ Thêm SoftDeletes
 
     protected $fillable = [
         'name', 'address', 'city_id', 'hotline', 'email', 'map_url',
