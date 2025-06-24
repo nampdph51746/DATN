a54d1354e415a0ac0d350ea610b08272b3820e25
giangvtph53035 <giangvtph53035>
2025-06-24 23:37:19 +0700

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['category_id', 'name','sku', 'description', 'image_url', 'product_type', 'is_active'];
+    protected $fillable = ['category_id', 'name', 'description', 'image_url', 'product_type', 'is_active'];
 
     protected $casts = [
         'product_type' => \App\Enums\ProductType::class,
