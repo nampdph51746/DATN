cab6d6826ea0f4cdcf390861a89f65b2b7ef1f1e
ThanhSonPH52957 <sont18573@gmail.com>
2025-08-12 22:38:11 +0700

    Cap nhat giao dien

--- resources/views/client/sign_in.blade.php
+++ resources/views/client/sign_in.blade.php
@@ -1,53 +1,52 @@
 @extends('layouts.client.client')
-<!-- ..............Sign-in............... -->
+<!-- ..............Đăng nhập / Đăng ký............... -->
 <link rel="stylesheet" type="text/css" href="client_assets/assets/css/sign-in.css">
 @section('content')
     <div class="container_signup_signin" id="container_signup_signin">
         <div class="form-container sign-up-container">
             <form name="sign-up-form" action="#" onsubmit="return signUpValidateForm()">
-                <h1>Create Account</h1>
+                <h1>Tạo tài khoản</h1>
                 <div class="social-container">
-                    <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#" class="social" title="Đăng ký bằng Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social" title="Đăng ký bằng Google"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social" title="Đăng ký bằng LinkedIn"><i class="fab fa-linkedin-in"></i></a>
                 </div>
-                <span>or use your email for registration</span>
-                <input name="sign-up-name" type="text" placeholder="Name" />
-                <input name="sign-up-email" type="email" placeholder="Email" />
-                <input name="sign-up-passwd" type="password" placeholder="Password" />
-                <button>Sign Up</button>
+                <span>hoặc đăng ký bằng email</span>
+                <input name="sign-up-name" type="text" placeholder="Nhập họ và tên" />
+                <input name="sign-up-email" type="email" placeholder="Nhập địa chỉ email" />
+                <input name="sign-up-passwd" type="password" placeholder="Nhập mật khẩu" />
+                <button>Đăng ký</button>
             </form>
         </div>
         <div class="form-container sign-in-container">
             <form name="sign-in-form" style="color: var(--theme-title);" action="#" onsubmit="return signInValidateForm()">
-                <h1>Sign in</h1>
+                <h1>Đăng nhập</h1>
                 <div class="social-container">
-                    <a href="#" class="social" style="color: var(--theme-title);"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#" class="social" style="color: var(--theme-title);"><i class="fab fa-google-plus-g"></i></a>
-                    <a href="#" class="social" style="color: var(--theme-title);"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="#" class="social" style="color: var(--theme-title);" title="Đăng nhập bằng Facebook"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social" style="color: var(--theme-title);" title="Đăng nhập bằng Google"><i class="fab fa-google-plus-g"></i></a>
+                    <a href="#" class="social" style="color: var(--theme-title);" title="Đăng nhập bằng LinkedIn"><i class="fab fa-linkedin-in"></i></a>
                 </div>
-                <span>or use your account</span>
-                <input name="sign-in-email" type="email" placeholder="Email" />
-                <input name="sign-in-passwd" type="password" placeholder="Password" />
-                <a href="#">Forgot your password?</a>
-                <button>Sign In</button>
+                <span>hoặc đăng nhập bằng email</span>
+                <input name="sign-in-email" type="email" placeholder="Nhập địa chỉ email" />
+                <input name="sign-in-passwd" type="password" placeholder="Nhập mật khẩu" />
+                <a href="#">Quên mật khẩu?</a>
+                <button>Đăng nhập</button>
             </form>
         </div>
         <div class="overlay-container">
             <div class="overlay">
                 <div class="overlay-panel overlay-left">
-                    <h1>Welcome Back!</h1>
-                    <p>To keep connected with us please login with your login details</p>
-                    <button class="ghost" id="signIn">Sign In</button>
+                    <h1>Chào mừng trở lại!</h1>
+                    <p>Để tiếp tục kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin của bạn</p>
+                    <button class="ghost" id="signIn">Đăng nhập</button>
                 </div>
                 <div class="overlay-panel overlay-right">
-                    <h1>Hello, Friend!</h1>
-                    <p>Register and book your tickets now!!!</p>
-                    <button class="ghost" id="signUp">Sign Up</button>
+                    <h1>Xin chào, bạn mới!</h1>
+                    <p>Hãy đăng ký và đặt vé ngay hôm nay!</p>
+                    <button class="ghost" id="signUp">Đăng ký</button>
                 </div>
             </div>
         </div>
     </div>
-        @include('client.footer.footer')
-
+    @include('client.footer.footer')
 @endsection
