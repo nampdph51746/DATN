41cc068b518507a052eca86965e9e771c9c93003
DuongPH52969FPT <duongttph52969@gmail.com>
2025-08-17 20:05:24 +0700

    fix sai validate max:5 (phải 10)

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -254,7 +254,7 @@ class MovieController extends Controller
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'genre_ids' => 'required|array|min:1',
             'genre_ids.*' => 'exists:genres,id',
-            'average_rating' => 'nullable|numeric|min:0|max:5',
+            'average_rating' => 'nullable|numeric|min:0|max:10',
             'description' => 'nullable|string',
         ], [
             // Thông báo lỗi giống bạn đã cung cấp
