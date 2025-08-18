3b0cd32017f4ecbb65181330f79e7fa016a19cc5
Doandan101 <143376226+Doandan101@users.noreply.github.com>
2025-08-18 17:20:20 +0700

    Thêm bảng db lọc nội dung bình luận

---
+++ database/migrations/2025_08_18_134201_remove_average_rating_from_movies_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            if (Schema::hasColumn('movies', 'average_rating')) {
+                $table->dropColumn('average_rating');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->float('average_rating')->nullable()->comment('Điểm đánh giá trung bình');
+        });
+    }
+};
