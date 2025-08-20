a9aea0a48794eefff2af5b67e5f4e5651cb87ec7
unknown <namhatsune@gmail.com>
2025-08-20 12:23:33 +0700

    Cap nhat giao dien Quan ly dien vien

--- resources/views/admin/actors/create.blade.php
+++ resources/views/admin/actors/create.blade.php
@@ -33,8 +33,8 @@
 
         <div class="col-xl-9 col-lg-8">
             <div class="card">
-                <div class="card-header" style="background: #ff6633; border-bottom: 1px solid #e55a2b;">
-                    <h4 class="card-title" style="color: #333333; font-weight: 600;">Thêm diễn viên mới</h4>
+                <div class="card-header">
+                    <h4 class="card-title">Thêm diễn viên mới</h4>
                 </div>
                 <div class="card-body">
                     <form id="actorCreateForm" action="{{ route('admin.actors.store') }}" method="POST" enctype="multipart/form-data">
